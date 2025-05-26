cc201bb844255bf0fc780a59ac6f9d8ad6021d77
Madhav088 <Luffy2314@proton.me>
2025-05-26 08:55:12 +0400

    Add files via upload

---
+++ Sample program 2.py
@@ -0,0 +1,7 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> l=12
+>>> b=5
+>>> A=l*b
+>>> print("Area of Rectangle=",A)
+Area of Rectangle= 60
